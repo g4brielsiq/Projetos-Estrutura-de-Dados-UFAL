2900cf477eb6e1ab0401ba817b6e9ea86c4649d9
Davi Domingos de Oliveira <odomingosdavi@gmail.com>
2025-09-24 19:29:08 -0300

    Update big_papao.c
    Adicionada: 
    - Função de Imprimir os funcionários

--- Big Papão/big_papao.c
+++ Big Papão/big_papao.c
@@ -41,9 +41,30 @@ Funcionario* mapa_habilidades() {
     return equipe;
 }
 
+void print_funcionarios(Funcionario* quadro) {
+
+    printf("\n");
+    for (int i = 0; i < NUM_FUNCIONARIOS; i++) {
+        printf("ID: %d\n", quadro[i].id);
+        printf("Nome: %s\n", quadro[i].nome);
+        printf("Funcoes: ");
+        for (int j = 0; j < quadro[i].num_funcoes; j++) {
+            printf("%s", quadro[i].funcoes[j]);
+            if (j < quadro[i].num_funcoes - 1) {
+                printf(", ");
+            }
+        }
+        printf("\nOcupado: %s\n", quadro[i].ocupado ? "Sim" : "Nao");
+        printf("------------------------\n");
+    }
+}
+
 
 int main()
 {
     Funcionario* Quadro = mapa_habilidades();
 
+    print_funcionarios(Quadro);
+
+    return 0;
 }
