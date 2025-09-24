e1f4a3475967a958655c22a88c4db3d74f1aa32c
Davi Domingos de Oliveira <odomingosdavi@gmail.com>
2025-09-24 19:30:22 -0300

    Update big_papao.c
    Alterado:
    - sandwich para sanduiche
    para facilitar a leitura

--- Big Papão/big_papao.c
+++ Big Papão/big_papao.c
@@ -13,9 +13,9 @@ typedef struct {
 
 typedef struct {
     int qtdBatataFrita;
-    int qtdSandwichSimples;
-    int qtdSandwichMedio;
-    int qtdSandwichElab;
+    int qtdsanduicheSimples;
+    int qtdsanduicheMedio;
+    int qtdsanduicheElab;
     int qtdRefrigerante;
     int qtdSuco;
     int qtdMilkShake;
@@ -24,17 +24,17 @@ typedef struct {
 
 Funcionario* mapa_habilidades() {
     static Funcionario equipe[NUM_FUNCIONARIOS] = {
-        {1, "Funcionario1", 3, {"sandwich", "batata_frita", "suco"}, 0},
-        {2, "Funcionario2", 2, {"sandwich", "batata_frita"}, 0},
-        {3, "Funcionario3", 1, {"sandwich"}, 0},
-        {4, "Funcionario4", 1, {"sandwich"}, 0},
-        {5, "Funcionario5", 1, {"sandwich"}, 0},
-        {6, "Funcionario6", 2, {"sandwich", "batata_frita"}, 0},
+        {1, "Funcionario1", 3, {"sanduiche", "batata_frita", "suco"}, 0},
+        {2, "Funcionario2", 2, {"sanduiche", "batata_frita"}, 0},
+        {3, "Funcionario3", 1, {"sanduiche"}, 0},
+        {4, "Funcionario4", 1, {"sanduiche"}, 0},
+        {5, "Funcionario5", 1, {"sanduiche"}, 0},
+        {6, "Funcionario6", 2, {"sanduiche", "batata_frita"}, 0},
         {7, "Funcionario7", 1, {"batata_frita"}, 0},
         {8, "Funcionario8", 2, {"bebidas", "montar_bandeja"}, 0},
         {9, "Funcionario9", 1, {"montar_bandeja"}, 0},
         {10, "Funcionario10", 2, {"separacao", "caixa"}, 0},
-        {11, "Funcionario11", 2, {"separacao", "sandwich"}, 0},
+        {11, "Funcionario11", 2, {"separacao", "sanduiche"}, 0},
         {12, "Funcionario12", 2, {"caixa", "bebidas"}, 0},
         {13, "Funcionario13", 1, {"caixa"}, 0}
     };
